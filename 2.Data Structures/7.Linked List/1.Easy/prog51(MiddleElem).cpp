cd95e5d83fefa9cb69cbdbad73e2eb9e1940970c
mohitsharma03 <03mohitsharma@gmail.com>
2023-03-11 21:23:08 +0530

    ll4

---
+++ 2.Data Structures/7.Linked List/1.Easy/prog51(MiddleElem).cpp
@@ -0,0 +1,79 @@
+//Find middle element in a Linked List(Torotoise-Hare method)
+//leetcode: https://leetcode.com/problems/middle-of-the-linked-list/description/
+//gfg: https://bit.ly/3dAjkn1
+///ref: https://takeuforward.org/data-structure/find-middle-element-in-a-linked-list/
+
+/***************************GFG soln**********************************/
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        Node *slow = head, *fast = head;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        return slow->data;
+    }
+};
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+
+/***************************Leetcode soln**********************************/
+  struct ListNode {
+      int val;
+     ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        return slow;
+    }
+};
